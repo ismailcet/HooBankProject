68715117c909a9bc39eed3f1180acacf24fba794
ismailcet <ismailcet3@gmail.com>
2022-09-28 23:26:40 +0300

    Header Component is finished

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { navLinks } from "../constans";
 import logo from "../assets/logo.svg";
 import menu from "../assets/menu.svg";
 const Navbar = () => {
-  return (
-    <nav className="header">
-      <div className="left-side">
-        <img src={logo} alt="Logo" className="nav-logo" />
-      </div>
-      <div className="right-side">
-        <img src={menu} alt="Menu Buttons" className="menu-img" />
+  const [isOpen, setIsOpen] = useState(false);
 
+  return (
+    <>
+      <nav className="header">
+        <div className="left-side">
+          <img src={logo} alt="Logo" className="nav-logo" />
+        </div>
+        <div className="hide-right-side">
+          <a className="menu-btn" onClick={() => setIsOpen(!isOpen)}>
+            <img src={menu} alt="Menu Buttons" className="menu-img" />
+          </a>
+        </div>
+        <div className="right-side">
+          <ul className="nav-items">
+            {navLinks.map((item) => (
+              <a className="nav-link" key={item.id}>
+                <li className="nav-item">{item.title}</li>
+              </a>
+            ))}
+          </ul>
+        </div>
+      </nav>
+      <div className={`drop-down ${isOpen ? "" : "close-items"}`}>
         <ul className="nav-items">
           {navLinks.map((item) => (
             <a className="nav-link" key={item.id}>
@@ -19,7 +35,7 @@ const Navbar = () => {
           ))}
         </ul>
       </div>
-    </nav>
+    </>
   );
 };
 
