5dff2105aee8f20696aa32189ad73fd3ae2cc465
ismailcet <ismailcet3@gmail.com>
2022-09-30 01:27:04 +0300

    Stats Component is finished

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Stats from "./components/Stats";
 import Business from "./components/Business";
 import Billing from "./components/Billing";
 import CardDetail from "./components/CardDetail";
@@ -14,6 +15,7 @@ function App() {
     <div className="App">
       <Navbar />
       <Hero />
+      <Stats />
       <Business />
       <Billing />
       <CardDetail />
--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,7 +1,16 @@
 import React from "react";
-
+import { stats } from "../constans";
 const Stats = () => {
-  return <div>Stats</div>;
+  return (
+    <div className="stats">
+      {stats.map((item) => (
+        <div className="stats-item" key={item.id}>
+          <h3 className="stats-number">{item.value}</h3>
+          <p className="stats-title">{item.title}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Stats;
